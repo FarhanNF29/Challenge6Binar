cbea003e097550aa068dca8770e4e6bf27e22a6b
FarhanNF29 <99528119+FarhanNF29@users.noreply.github.com>
2023-11-09 11:11:15 +0700

    Inpect code, remove unused code, and add missing atribute

--- app/src/main/java/com/example/challenge3binar/network/service/ApiService.kt
+++ app/src/main/java/com/example/challenge3binar/network/service/ApiService.kt
@@ -10,7 +10,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
-@Suppress("unused", "unused")
+@Suppress("unused")
 interface ApiService {
 
     @GET("listmenu")
